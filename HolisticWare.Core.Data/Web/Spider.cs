6de28ec1d9c0348d82615f7f80ca39a214d9936b
moljac <mcvjetko@holisticware.net>
2015-01-13 16:37:53 +0100

    pcl project added

---
+++ HolisticWare.Core.Data/Web/Spider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HolisticWare.Core.Data.Web
+{
+	public partial class Spider
+	{
+		public async void Crawl(string url)
+		{
+
+		}
+	}
+}
